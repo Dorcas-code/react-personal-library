97445441a033b6a951a8364f280c101f15dd63b5
Dorcas <55081515+Dorcas-code@users.noreply.github.com>
2026-06-25 10:17:11 +0000

    faviorite try to loop out data

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,13 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router";
 import { useTheme } from "../context/ThemeContext";
 import Navigation from "../components/Navigation";
 import ThemeToggleButton from "../components/ThemeToggleButton";
 import { useLibrary} from "../context/LibraryContext";
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 export default function FavoriteBookPage() {
+  const [books, setBooks] = useState([]);
+  const [ datas, setDatas] = useState([]);
     const { theme, toggleTheme } = useTheme();
-     const { value } =  useLibrary();
-   console.log( value);
+     const { value} =  useLibrary();
+const id = value[0];
+console.log(typeof(value));
+//need to parse the value!
+ const apiKey = import.meta.env.VITE_MY_API_KEY;
+        useEffect(() => {
+        //  value.map((id) => {
+          fetch(`https://www.googleapis.com/books/v1/volumes?q=${id}&key=${apiKey}`)
+            .then((res) => {
+            return res.json();
+            })
+            .then((data) => {
+                setBooks(data.items);
+        
+            })
+          // })
+    },[])
 
   return (
  <>
@@ -15,7 +35,7 @@ export default function FavoriteBookPage() {
            
         <Navigation />
             <h1>My favorite books</h1>
-        <h1>{value}</h1>
+      
        </div>
 </>
   );
