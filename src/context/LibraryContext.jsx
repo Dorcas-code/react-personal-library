046da5f10cd425dfaaf933a07ec087b08ff3c954
Dorcas <55081515+Dorcas-code@users.noreply.github.com>
2026-06-25 09:51:40 +0000

    Loaded favorite books data from another component to favorite page

--- src/context/LibraryContext.jsx
+++ src/context/LibraryContext.jsx
@@ -2,41 +2,25 @@ import { createContext, useContext, useState,useEffect  } from 'react';
 import useAPI from "../hooks/useAPI";
 
 //1. Createing the context
-const LibraryContext = createContext([]);
+const LibraryContext = createContext();
 
 // 2. Context Provider Component
 export const LibraryProvider = ({ children }) => {
     
+  const [value, setValue] = useState(() => localStorage.getItem('favorites') || '');
 
-const apiKey = import.meta.env.VITE_MY_API_KEY;
-const url = `https://www.googleapis.com/books/v1/volumes?q=intitle:Harry-Potter&key=${apiKey}`;
+  const updateValue = (newValue) => {
+    setValue(newValue);
+    localStorage.setItem('sharedData', newValue);
+  };
 
 
- const [books, setBooks] = useState([]);
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    
-      fetch(url)
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-        
-          setBooks(data.items);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setIsLoading(false);
-        });
-    }, [url])
-
+  
 
   return (
-    <LibraryContext.Provider value={{books, isLoading, error}}>
+    <LibraryContext.Provider value={{value, updateValue}}>
         {children}
         </LibraryContext.Provider>
   );
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,24 +2,21 @@ import { useNavigate } from "react-router";
 import { useTheme } from "../context/ThemeContext";
 import Navigation from "../components/Navigation";
 import ThemeToggleButton from "../components/ThemeToggleButton";
+import { useLibrary} from "../context/LibraryContext";
 
 export default function FavoriteBookPage() {
     const { theme, toggleTheme } = useTheme();
+     const { value } =  useLibrary();
+   console.log( value);
+
   return (
  <>
-<div  style={{
-
-                             width:"100%",
-                             height:"100vh",
-                           
-                  backgroundColor: theme === "light" ? "black" : "white"
-                }}>
+       <div >
            
         <Navigation />
-        <h1  style={{
-                color: theme === "light" ? "white" : "black"
-                }}>My Facorite Books</h1>
-         </div>
+            <h1>My favorite books</h1>
+        <h1>{value}</h1>
+       </div>
 </>
   );
 }
