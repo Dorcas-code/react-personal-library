4bc208227c12a4166202d5b782c72deb1f3309b1
Dorcas <55081515+Dorcas-code@users.noreply.github.com>
2026-06-26 06:46:41 +0000

    added setting in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/react-personal-library/', 
+    base: '/react-personal-library', 
   plugins: [react(),
+    ghPages(),
     tailwindcss(),],
 })
