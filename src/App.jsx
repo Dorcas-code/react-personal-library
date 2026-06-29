b01c01eaa55bacad72604b5990f1ab3a9102413d
Dorcas <dorcaslam30@gmail.com>
2026-06-29 08:02:04 +0000

    csc

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
         <LibraryProvider>
                <ThemeProvider>
- <HashRouter basename={import.meta.env.BASE_URL}>
+ <HashRouter>
       <Routes>
  
           <Route path="/" element={<LibraryPage />} >
