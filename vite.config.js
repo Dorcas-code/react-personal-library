dca3aa70299d1a30e527c8b6caf43808dc5c6203
Dorcas <dorcaslam30@gmail.com>
2026-06-29 06:23:21 +0000

    add deploy yml file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/react-personal-library', 
+  
   plugins: [react()],
+   base: "/react-personal-library/", 
    
 })
