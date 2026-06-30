75982ab696757ccdc9d96d0a787c1b869cb3358b
Dorcas <dorcaslam30@gmail.com>
2026-06-30 02:13:00 +0000

    change import of useParams  from react router dom

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navigation from "../components/Navigation";
-import { useParams } from "react-router";
+import { useParams } from 'react-router-dom';
 
 
 
